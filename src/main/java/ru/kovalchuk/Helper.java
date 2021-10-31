6292a93e1ffdb5cfe5bd824f9a4cd7d627b50596
migacheva <anutamig@mail.ru>
2021-10-31 18:13:47 +0300

    Mini refactoring

--- src/main/java/ru/kovalchuk/Helper.java
+++ src/main/java/ru/kovalchuk/Helper.java
@@ -4,9 +4,9 @@ public class Helper {
     static boolean check_exist_task(String[][] list_task) {
         if (list_task[1][0].equals("")){
             System.out.println("Не найдено ни одной задачи");
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
     static void toggle_task(String in_progress, String done, String[][] list_task, Integer id_task) {
@@ -28,13 +28,12 @@ public class Helper {
         }
         System.out.println();
     }
-
     static void print_in_progress_tasks(String in_progress, String[][] list_task) {
         System.out.println("Список невыполненных задач: ");
-        for (int i = 1; i < list_task.length; ++i) {
-            for(int j = 0; j < list_task[i].length; ++j) {
-                if (list_task[i][1].equals(in_progress)) {
-                    System.out.print(list_task[i][j]);
+        for (String[] task : list_task) {
+            for(String param : task) {
+                if (task[1].equals(in_progress)) {
+                    System.out.print(param);
                 }
             }
             System.out.println();
@@ -43,23 +42,23 @@ public class Helper {
 
     static void print_all_tasks(String[][] list_task) {
         System.out.println("Список абсолютно всех задач: ");
-        for (int i = 0; i < list_task.length; ++i) {
-            for(int j = 0; j < list_task[i].length; ++j) {
-                System.out.print(list_task[i][j]);
+        for (String[] task : list_task) {
+            for (String param : task) {
+                System.out.print(param);
             }
             System.out.println();
         }
     }
 
     static int add_value_in_todo(int iter, String[][] list_task, String in_progress, String task_name) {
-        if (!(task_name.equals("") | task_name.equals(" ") | task_name.equals("\n")))
-            for (int i = iter; i< iter +1; i++) {
+        if (task_name.isBlank()) {
+            System.out.println("Вводить пустые строки, пробелы, перенос строки и обижать котяток нельзя.");
+        } else {
+            for (int i = iter; i < iter + 1; i++) {
                 list_task[i][0] = Integer.toString(i);
                 list_task[i][1] = in_progress;
                 list_task[i][2] = task_name;
             }
-        else{
-            System.out.println("Вводить пустые строки, пробелы, перенос строки и обижать котяток нельзя.");
         }
         iter++;
         return iter;
