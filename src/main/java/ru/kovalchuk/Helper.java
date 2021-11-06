31b6c622a456ef2c5441e658650df8a3ed43399f
migacheva <anutamig@mail.ru>
2021-11-06 23:22:00 +0300

    Исправление замечаний ч1

--- src/main/java/ru/kovalchuk/Helper.java
+++ src/main/java/ru/kovalchuk/Helper.java
@@ -6,37 +6,38 @@ import java.util.List;
 
 
 public class Helper {
-    private static final String in_progress = ". [ ] "; // задача не выполнена
+    private static final String inProgress = ". [ ] "; // задача не выполнена
     private static final String done = ". [v] "; // задача выполнена
 
-    static boolean check_exist_task(List<Task> list_task) {
-        if (list_task.size() == 0){
+    static boolean checkExistTask(List<Task> taskList) {
+        if (taskList.size() == 0){
             System.out.println("Не найдено ни одной задачи");
             return false;
         }
         return true;
     }
 
-    static void toggleTask(BufferedReader line, List<Task> list_task) throws IOException {
-        if (Helper.check_exist_task(list_task)) {
+    static void toggleTask(BufferedReader line, List<Task> taskList) throws IOException {
+        if (Helper.checkExistTask(taskList)) {
             // System.out.println("Введите id задачи: ");
-            int id_task = Integer.parseInt(line.readLine()) - 1;
             try {
-                Task task = list_task.get(id_task);
+                int idTask = Integer.parseInt(line.readLine()) - 1;
+                Task task = taskList.get(idTask);
                 task.setDone(!task.isDone());
-                printTaskInfo(id_task, task);
-            }
-            catch (IndexOutOfBoundsException e) {
+                printTaskInfo(idTask, task);
+            } catch (IndexOutOfBoundsException e) {
                 System.out.println("Задачи с таким id нет");
+            } catch (NumberFormatException e){
+                System.out.println("Необходимо ввести id задачи цифрами");
             }
         }
     }
 
-    static void printInProgressTasks(List<Task> list_task) {
-        if (Helper.check_exist_task(list_task)) {
+    static void printInProgressTasks(List<Task> taskList) {
+        if (Helper.checkExistTask(taskList)) {
             // System.out.println("Список невыполненных задач: ");
-            for (int i = 0; i < list_task.size(); i++) {
-                Task task = list_task.get(i);
+            for (int i = 0; i < taskList.size(); i++) {
+                Task task = taskList.get(i);
                 if (!task.isDone()) {
                     printTaskInfo(i, task);
                 }
@@ -44,65 +45,67 @@ public class Helper {
         }
     }
 
-    static void printAllTasks(List<Task> list_task) {
-        if (Helper.check_exist_task(list_task))
+    static void printAllTasks(List<Task> taskList) {
+        if (Helper.checkExistTask(taskList))
         {
             // System.out.println("Список абсолютно всех задач: ");
-            for (int i = 0; i < list_task.size(); i++) {
-                printTaskInfo(i, list_task.get(i));
+            for (int i = 0; i < taskList.size(); i++) {
+                printTaskInfo(i, taskList.get(i));
             }
         }
     }
 
-    static void addValueInTodo(BufferedReader line, List<Task> list_task) throws IOException {
+    static void addValueInTodo(BufferedReader line, List<Task> taskListListtask) throws IOException {
 //        System.out.print("Введите название задачи:  ");
         String taskName = line.readLine();
         if (taskName.isBlank()) {
             System.out.println("Вводить пустые строки, пробелы, перенос строки и обижать котяток нельзя.");
         } else {
-            list_task.add(new Task(taskName));
+            taskListListtask.add(new Task(taskName));
         }
     }
 
-    public static void deleteTask(BufferedReader line, List<Task> list_task) throws IOException {
-        if (Helper.check_exist_task(list_task)) {
+    public static void deleteTask(BufferedReader line, List<Task> taskList) throws IOException {
+        if (Helper.checkExistTask(taskList)) {
             // System.out.println("Введите id задачи: ");
-            int id_task = Integer.parseInt(line.readLine()) - 1;
             try {
-                list_task.remove(id_task);
+                int idTask = Integer.parseInt(line.readLine()) - 1;
+                taskList.remove(idTask);
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Задачи с таким id нет");
+            } catch (NumberFormatException e){
+                System.out.println("Необходимо ввести id задачи цифрами");
             }
         }
     }
 
-    public static void editTask(BufferedReader line, List<Task> list_task) throws IOException {
-        if (Helper.check_exist_task(list_task)) {
-            // System.out.println("Введите id задачи: ");
-            int id_task = Integer.parseInt(line.readLine()) - 1;
-            // System.out.println("Введите новое название задачи: ");
-            String new_value = line.readLine();
+    public static void editTask(BufferedReader line, List<Task> taskList) throws IOException {
+        if (Helper.checkExistTask(taskList)) {
             try {
-                list_task.get(id_task).setName(new_value);
+                // System.out.println("Введите id задачи: ");
+                int idTask = Integer.parseInt(line.readLine()) - 1;
+                // System.out.println("Введите новое название задачи: ");
+                String newValue = line.readLine();
+                taskList.get(idTask).setName(newValue);
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Задачи с таким id нет");
+            } catch (NumberFormatException e){
+                System.out.println("Необходимо ввести id задачи");
             }
         }
     }
 
-    public static void searchTask(BufferedReader line, List<Task> list_task) throws IOException {
-        if (Helper.check_exist_task(list_task)) {
+    public static void searchTask(BufferedReader line, List<Task> taskList) throws IOException {
+        if (Helper.checkExistTask(taskList)) {
             // System.out.println("Текст для поиска: ");
             String search = line.readLine();
-
             boolean somethingSearched = false;
-            for (int i = 0; i < list_task.size(); i++) {
-                if (list_task.get(i).getName().contains(search)) {
-                    printTaskInfo(i, list_task.get(i));
+            for (int i = 0; i < taskList.size(); i++) {
+                if (taskList.get(i).getName().contains(search)) {
+                    printTaskInfo(i, taskList.get(i));
                     somethingSearched = true;
                 }
             }
-
             if (!somethingSearched) {
                 System.out.println("Ничего не найдено");
             }
@@ -110,17 +113,20 @@ public class Helper {
     }
 
     public static void printTaskInfo(int id, Task task) {
-        System.out.println((id + 1) + " " + (task.isDone() ? done : in_progress) + " " + task.getName());
+        System.out.println((id + 1) + " " + (task.isDone() ? done : inProgress) + " " + task.getName());
     }
 
     static void helper() {
         System.out.print("""
-    Вам доступны следующие команды:
+    Список доступных команд:
      help - все доступные команды
      add - добавление новой задачи
      print - получение невыполненных списка задач
      print all - получение всех задач
      toggle - переключение состояния задачи на противоположное
+     edit - изменение задачи
+     search - поиск задач
+     delete - удаление задачи
      quit - завершение работы программы
     """);
     }
--- src/main/java/ru/kovalchuk/Main.java
+++ src/main/java/ru/kovalchuk/Main.java
@@ -7,30 +7,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader line = new BufferedReader(new InputStreamReader(System.in));
-        List<Task> list_task = new ArrayList<>();
+        List<Task> taskList = new ArrayList<>();
         try (Scanner in = new Scanner(System.in)) {
             while(true) {
                 String cmd = in.nextLine();
-                if (cmd.equals("help")) {
-                    Helper.helper();
-                } else if (cmd.equals("add")) {
-                    Helper.addValueInTodo(line, list_task);
+                if (cmd.equals("add")) {
+                    Helper.addValueInTodo(line, taskList);
                 } else if (cmd.equals("print")) {
-                    Helper.printInProgressTasks(list_task);
+                    Helper.printInProgressTasks(taskList);
                 } else if (cmd.equals("print all"))  {
-                    Helper.printAllTasks(list_task);
+                    Helper.printAllTasks(taskList);
                 } else if (cmd.equals("toggle")) {
-                    Helper.toggleTask(line, list_task);
+                    Helper.toggleTask(line, taskList);
                 }  else if (cmd.equals("search")){
-                    Helper.searchTask(line, list_task);
+                    Helper.searchTask(line, taskList);
                 } else if (cmd.equals("edit")){
-                    Helper.editTask(line, list_task);
+                    Helper.editTask(line, taskList);
                 } else if (cmd.equals("delete")){
-                    Helper.deleteTask(line, list_task);
+                    Helper.deleteTask(line, taskList);
                 }else if (cmd.equals("quit")) {
                     in.close();
                 } else {
                     System.out.println("Такой команды нет");
+                    Helper.helper();
                 }
             }
         }
