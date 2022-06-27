f6aef87fd5029fb2d67ecb70fd2fe6fbba7ac539
migacheva <migacheva9>
2022-06-27 19:55:36 +0300

    Добавление валидации

--- src/main/java/ru/kovalchuk/task/model/AddTaskRequest.java
+++ src/main/java/ru/kovalchuk/task/model/AddTaskRequest.java
@@ -1,6 +1,12 @@
 package ru.kovalchuk.task.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
 public class AddTaskRequest {
+    @NotBlank
+    @Length(max = 25)
     private String nameTask;
 
     public String getNameTask() {
--- src/main/java/ru/kovalchuk/task/model/EditTaskRequest.java
+++ src/main/java/ru/kovalchuk/task/model/EditTaskRequest.java
@@ -1,10 +1,15 @@
 package ru.kovalchuk.task.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 
 //дата - генерирует геттеры и сеттеры при компиляции (Generates getters for all fields, a useful toString method, and hashCode and equals implementations that check all non-transient fields. Will also generate setters for all non-final fields, as well as a constructor.)
 @Data
 public class EditTaskRequest {
+    @NotBlank
+    @Length(max = 25)
     private String newTaskName;
 
     private boolean changeStatus;
